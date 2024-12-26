c4b82a05490c86970758b967d5ea2c983a3b5de0
moun_sovongdy <sovongdy.m@gmail.com>
2024-12-26 17:19:46 +0700

    continue

--- dist/script.js
+++ dist/script.js
@@ -1,9 +1,11 @@
+
+// Note: This file is used for demo purposes only
 function Menu(e) {
     let list = document.querySelector('ul');
     e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')) : (e.name = "menu", list.classList.remove('top-[80px]'), list.classList.remove('opacity-100'))
 }
 
-// Product data
+// Reference to the Product Grid Container
 const products = [
     { id: 1, title: "Product 1", description: "Description for product 1", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL-110926-Injector-Cleaner-Diesel-1L_png.png&w=1080&q=75" },
     { id: 2, title: "Product 2", description: "Description for product 2", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112828_AUTO%2520COOL%2520ESSENTIAL%25204X5L_png.png&w=1080&q=75" },
@@ -13,29 +15,36 @@ const products = [
     { id: 6, title: "Product 6", description: "Description for product 6", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112650_AUTO%2520COOL%2520G12%2520EVO%2520-37%25C2%25B0C%252012X1L_png.png&w=1080&q=75" },
     { id: 7, title: "Product 7", description: "Description for product 7", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112650_AUTO%2520COOL%2520G12%2520EVO%2520-37%25C2%25B0C%252012X1L_png.png&w=1080&q=75" },
     { id: 8, title: "Product 8", description: "Description for product 8", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112650_AUTO%2520COOL%2520G12%2520EVO%2520-37%25C2%25B0C%252012X1L_png.png&w=1080&q=75" },
-    { id: 1, title: "Product 1", description: "Description for product 1", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL-110926-Injector-Cleaner-Diesel-1L_png.png&w=1080&q=75" },
-    { id: 2, title: "Product 2", description: "Description for product 2", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112828_AUTO%2520COOL%2520ESSENTIAL%25204X5L_png.png&w=1080&q=75" },
-    { id: 3, title: "Product 3", description: "Description for product 3", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112828_AUTO%2520COOL%2520ESSENTIAL%25204X5L_png.png&w=1080&q=75" },
-    { id: 4, title: "Product 4", description: "Description for product 4", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112650_AUTO%2520COOL%2520G12%2520EVO%2520-37%25C2%25B0C%252012X1L_png.png&w=1080&q=75" },
+    { id: 9, title: "Product 9", description: "Description for product 9", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL-110926-Injector-Cleaner-Diesel-1L_png.png&w=1080&q=75" },
+    { id: 1, title: "Product 10", description: "Description for product 10", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112828_AUTO%2520COOL%2520ESSENTIAL%25204X5L_png.png&w=1080&q=75" },
+    { id: 2, title: "Product 11", description: "Description for product 11", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112828_AUTO%2520COOL%2520ESSENTIAL%25204X5L_png.png&w=1080&q=75" },
+    { id: 3, title: "Product 12", description: "Description for product 12", image: "https://www.motul.com/_next/image?url=https%3A%2F%2Fazupim01.motul.com%2Fmedia%2FmotulData%2FIM%2Fbigweb%2FMOTUL_112650_AUTO%2520COOL%2520G12%2520EVO%2520-37%25C2%25B0C%252012X1L_png.png&w=1080&q=75" },
 ];
 
 // Reference to the Product Grid Container
-const productsPerPage = 4;
+const productsPerPage = 8;
 let currentPage = 1;
+
 
 const productGrid = document.getElementById('productGrid');
 if (productGrid) {
     fetchProducts(currentPage);
 }
 
+// Function to fetch products based on the current page
 function fetchProducts(page) {
     const start = (page - 1) * productsPerPage;
     const end = start + productsPerPage;
     const paginatedProducts = products.slice(start, end);
 
-    displayProducts(paginatedProducts);
+    if (paginatedProducts.length > 0) {
+        displayProducts(paginatedProducts);
+    } else {
+        productGrid.innerHTML = '<p>No products available.</p>';
+    }
 }
 
+// Function to display products in the grid
 function displayProducts(products) {
     const productHTML = products.map(product => `
         <a href="product-detail.html?id=${product.id}" class="block max-w-sm bg-white rounded-lg shadow-md overflow-hidden transform transition-transform hover:scale-105">
@@ -49,11 +58,13 @@ function displayProducts(products) {
     productGrid.innerHTML = productHTML;
 }
 
+// Function to navigate to the next or previous page
 function goToPage(page) {
     currentPage = page;
     fetchProducts(page);
 }
 
+// Function to navigate to the next or previous page
 function prevPage() {
     if (currentPage > 1) {
         currentPage--;
@@ -61,6 +72,7 @@ function prevPage() {
     }
 }
 
+// Function to navigate to the next or previous page
 function nextPage() {
     if (currentPage * productsPerPage < products.length) {
         currentPage++;
@@ -74,6 +86,7 @@ function getProductId() {
     return parseInt(params.get('id'), 10);
 }
 
+// Function to Fetch Product Details
 function changeBgColor(element) {
     // Reset background color for all pagination buttons
     const buttons = document.querySelectorAll('#pagination li');
@@ -105,6 +118,18 @@ document.addEventListener('DOMContentLoaded', function () {
 function addToCart() {
     alert("Product added to cart!");
 }
+
+//const modal = document.getElementById('contactModal');
+function toggleModal(modalId) {
+    const modal = document.getElementById(modalId);
+    modal.classList.toggle('hidden');
+}
+
+// Close the modal when the user clicks outside of the modal
+document.querySelector('a[href="#contact"]').addEventListener('click', function(event) {
+    event.preventDefault();
+    toggleModal('contactModal');
+});
 
 // Mobile menu toggle function
 function onToggleMenu(e) {
